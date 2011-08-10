466639d52e12e6565b64ca8223625e909f3f980e
Daniel Dosen <ddosen@gmail.com>
2011-08-10 09:41:08 -0700

    adding more sample wont and must verbage...

--- test/specs/test_activities.rb
+++ test/specs/test_activities.rb
@@ -7,17 +7,39 @@ describe "Activity" do
     @article = Article.new(:title => 'test')
   end
  
+  # playing with minitest syntax...
   describe "when asked about an article" do
+    # sample postivie expectations:
     it "must be have a saved title" do
       @article.title.must_equal 'test'
     end
 
+    it "must have an ariticle of type string" do
+      @article.title.must_be_kind_of(String).must_equal true
+    end
+
+    it "must be an Article instance" do
+      @article.must_be_instance_of(Article)
+    end
+    it "must match a valid title" do
+      @article.title.must_match(/\w+/).must_equal true
+    end
+
+    it "must have a title with the letter e in it" do
+      @article.title.must_include('e')
+    end
+
+    # sample negative expecations
     it "must not have a blank title" do
       @article.title.wont_be_empty
     end
 
     it "won't have a silly title" do
       @article.title.wont_equal 'blah, blah, blah, Ginger'
+    end
+
+    it "won't inclue a reference to politics" do
+      @article.title.wont_include('politics')
     end
   end
 
